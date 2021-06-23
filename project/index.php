27c8f8da3171fe6110228565b2a3dc6a80ae2869
supersl555 <61551225+supersl555@users.noreply.github.com>
2021-06-23 17:25:15 +0600

    Add files via upload

---
+++ project/index.php
@@ -0,0 +1,225 @@
+.<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title>PORTEN</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css"/>
+	<meta name="viewport" content="width=device-width">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=1920">
+</head>
+<header>
+	<div class="container">
+		<img src="img/Vector.png" class="photo_container">
+		<span class="text_container">8 (812) 123-45-67    |    Работаем 7 дней в неделю    |    9:00 — 18:00  </span>
+		<a href="#" class="exit_container"><img src="img/exit.png"></a>
+		<div class="block_entry_container">
+			<a href="#" class="entry_container">Войти / Регистрация</a>
+		</div>
+	</div>
+	<div class="shapka">
+		<h1 class="text_shapka">PORTEN</h1>
+		<button class="button_shapka action">ПОНРАВИЛОСЬ</button>
+		<button class="button_shapka2 action">ЛИЧНЫЙ КАБИНЕТ</button>
+		<button class="button_shapka3 action">НАСТРОЙКИ</button>
+		<a href="#"><img src="img/buy.png" class="buy_shapka"></a>
+		<a href="#"><img src="img/search.png" class="search_shapka"></a>
+	</div>
+</header>
+<main>
+	<div class="logo_main">
+		<div class="rect_logo_main">
+			<h1 class="logo_text_main">PORTEN</h1>
+			<div class="line"></div>
+			<span class="under_logo_main">САНКТ-ПЕТЕРБУРГ</span>
+		</div>
+		<div class="on_text_rect">
+			<div class="text_under_rect">
+				<b>LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. PURUS INTERDUM PURUS, EST TORTOR PULVINAR UT IN. FRINGILLA A DIAM ENIM SED JUSTO, SED IACULIS SAGITTIS. TORTOR ID EU INTERDUM NEC UT IACULIS. PENATIBUS ULLAMCORPER ULTRICIES MORBI IPSUM SEM METUS PHARETRA, MI. TORTOR NIBH MAGNA FEUGIAT ID NUNC, DUI NISL VIVERRA.</b>
+			</div>
+		</div>
+	</div>
+</main>
+<section>
+	<div class="shop">
+		<div class="main_text_shop">
+			<h2 align="center">СЕЗОН 2020/21</h2>
+		</div>
+		<div class="line_shop"></div>
+		<div class="main_catalog_shop">
+			<div class="catalog_shop">
+				<div class="item_shop">
+					<img class="img_shop" src="img/watch.png">
+				</div>
+				<div class="text_shop">
+					<span class="name_shop">Louis XVI ATHOS</span>
+				<div class="price_shop">
+					<span>165 000 руб.</span>
+				</div>
+			</div>
+			</div>
+			<div class="catalog_shop">
+				<div class="item_shop">
+					<img class="img_shop" src="img/watch.png">
+				</div>
+				<div class="text_shop">
+					<span class="name_shop">Louis XVI ATHOS</span>
+					<div class="price_shop">
+						<span>165 000 руб.</span>
+					</div>
+				</div>
+			</div>
+			<div class="catalog_shop">
+				<div class="item_shop">
+					<img class="img_shop" src="img/watch.png">
+				</div>
+				<div class="text_shop">
+					<span class="name_shop">Louis XVI ATHOS</span>
+				<div class="price_shop">
+					<span>165 000 руб.</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section>
+	<div class="new_collection">
+		<h2 class="text_collection">НОВАЯ КОЛЛЕКЦИЯ</h2>
+		<div class="line_collection"></div>
+		<button class="button_collection">КАТАЛОГ</button>
+	</div>
+</section>
+<section>
+	<img class="block_img"src="img/Rectangle 9.png">
+</section>
+<section>
+	<div class="info_collection">
+		<div class="block_collection_info">
+			<h2 class="main_text_collection_info">КОЛЛЕКЦИЯ 2018</h2>
+			<div class="line_info_collection"></div>
+			<div class="main_info"><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non rutrum ornare ut mattis habitant dui arcu. Sagittis amet nunc ut neque quis nibh arcu. Vivamus vestibulum nisi et venenatis sed scelerisque magna consectetur. Amet convallis quis gravida facilisis vulputate. Faucibus facilisi habitasse ipsum interdum dictum aliquet. Velit quis ullamcorper pulvinar nulla malesuada integer. Aenean praesent viverra nulla nullam natoque volutpat curabitur auctor. Viverra viverra ullamcorper scelerisque risus dignissim egestas. Id aliquam a aliquam egestas leo orci pharetra sed diam. </b></div>
+			<button class="button_info">ПОСМОТРЕТЬ КОЛЛЕКЦИЮ</button>
+		</div>
+	</div>
+</section>
+<article>
+	<div class="new">
+		<h1 class="main_text_new">НОВЫЕ ПОСТУПЛЕНИЯ</h1>
+		<div class="line_new"></div>
+		<div class="catalog_new">
+			<div class="catalog_shop">
+				<div class="item_shop">
+					<img class="img_shop" src="img/watch.png">
+				</div>
+				<div class="text_shop">
+					<span class="name_shop">Louis XVI ATHOS</span>
+					<div class="price_shop">
+						<span>165 000 руб.</span>
+					</div>
+				</div>
+			</div>
+			<div class="catalog_shop">
+				<div class="item_shop">
+					<img class="img_shop" src="img/watch.png">
+				</div>
+				<div class="text_shop">
+					<span class="name_shop">Louis XVI ATHOS</span>
+					<div class="price_shop">
+						<span>165 000 руб.</span>
+					</div>
+				</div>
+			</div>
+			<div class="catalog_shop">
+				<div class="item_shop">
+					<img class="img_shop" src="img/watch.png">
+				</div>
+				<div class="text_shop">
+					<span class="name_shop">Louis XVI ATHOS</span>
+					<div class="price_shop">
+						<span>165 000 руб.</span>
+					</div>
+				</div>
+			</div>
+			<div class="catalog_shop">
+				<div class="item_shop">
+					<img class="img_shop" src="img/watch.png">
+				</div>
+				<div class="text_shop">
+					<span class="name_shop">Louis XVI ATHOS</span>
+					<div class="price_shop">
+						<span>165 000 руб.</span>
+					</div>
+				</div>
+			</div>
+			<div class="catalog_shop">
+				<div class="item_shop">
+					<img class="img_shop" src="img/watch.png">
+				</div>
+				<div class="text_shop">
+					<span class="name_shop">Louis XVI ATHOS</span>
+					<div class="price_shop">
+						<span>165 000 руб.</span>
+					</div>
+				</div>
+			</div>
+			<div class="catalog_shop ">
+				<div class="item_shop">
+					<img class="img_shop" src="img/watch.png">
+				</div>
+				<div class="text_shop">
+					<span class="name_shop">Louis XVI ATHOS</span>
+					<div class="price_shop">
+						<span>165 000 руб.</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
+<footer>
+	<div class="first_footer">
+		<h2 class="main_text_footer">НАШИ БРЕНДЫ</h2>
+		<div class="line_new"></div>
+		<div class="img_footer">
+			<img class="block_img_footer" src="img/Rectangle 2.png">
+			<img class="block_img_footer" src="img/Rectangle 2.png">
+			<img class="block_img_footer" src="img/Rectangle 2.png">
+			<img class="block_img_footer" src="img/Rectangle 2.png">
+		</div>
+	</div>
+	<div class="second_footer">
+		<div class="text_second_footer">
+			<h2 class="main_text_second_footer">О МАГАЗИНЕ</h2>
+			<div class="main_text_second_footer_2">
+				<b>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi semper viverra nunc cursus tortor lectus nunc nulla nibh. Egestas amet consectetur vel vitae aliquam dictum suspendisse. Lobortis eget consequat, tellus et et sed turpis. Pretium quisque vitae, amet, porttitor odio ultricies massa pharetra leo. Et ipsum urna fames in sit mi ultrices nisi, nunc.</b>
+			</div>
+		</div>
+		<div class="catalog_second_footer">
+			<h2 class="block_text_second_footer">КАТЕГОРИИ</h2>
+			<div class="block_main_text_second_footer_2">
+				<button class="watch_second_footer">часы</button>
+				<button class="bracelets">браслеты</button>
+				<button class="belts">ремни</button>
+				<button class="jewelry">ювелирные изделия</button>
+				<button class="cufflinks">запонки</button>
+			</div>
+		</div>
+		<div class="catalog_third_footer">
+			<h2 class="block_text_second_footer">РАССЫЛКА</h2>
+			<div class="block_main_text_third_footer_2">
+				<b>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi semper viverra nunc cursus tortor lectus nunc nulla nibh.</b>
+			</div>
+			<input class="input" type="search" name="q" placeholder="Ваша почта">
+			<button class="button_footer">ПОДПИСАТЬСЯ</button>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="text_footer">
+			<span>© 2020 Все права защищены</span>
+		</div>
+		<div class="text_footer_last">
+			<span>Сделано by superesl community</span>
+		</div>
+	</div>
+</footer>
